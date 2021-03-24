9d34e686370235b3259a92a484953a6331a05379
grzegorz <grzesiu15432@gmail.com>
2021-03-24 15:28:56 +0100

    Make newline separator test pass

--- src/main/java/pl/agh/xp/StringCalculator/Calculator.java
+++ src/main/java/pl/agh/xp/StringCalculator/Calculator.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 
 public class Calculator {
     public String add(String s) {
-       if(s == null || s.isBlank())
-           return "0";
-       var splitted = s.split(",");
-       var numbers = Arrays.stream(splitted).map(Float::parseFloat);
-       var sum = numbers.reduce(0f, Float::sum);
-       return new DecimalFormat("0.#").format(sum);
+        var separator = "[,\\n]";
+        if (s == null || s.isBlank())
+            return "0";
+        var splitted = s.split(separator);
+        var numbers = Arrays.stream(splitted).map(Float::parseFloat);
+        var sum = numbers.reduce(0f, Float::sum);
+        return new DecimalFormat("0.#").format(sum);
     }
 }
