469f5e7d4c5f85e833a7166a68feec67d9c80b77
Lukasz Puton <lukasputon@gmail.com>
2021-03-24 15:00:02 +0100

    Test for adding two numbers

---
+++ src/test/java/pl/agh/xp/Lab2/Calculator/CalculatorTest.java
@@ -0,0 +1,18 @@
+package pl.agh.xp.Lab2.Calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void testAddUpTwoNumberWhenStringValid(){
+        Calculator calculator = new Calculator();
+
+        String numbersToAdd = "1,2";
+        String result = calculator.add(numbersToAdd);
+        assertEquals("3", result);
+    }
+
+}
