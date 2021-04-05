61c482aa32a312c189ec491dbc176c287c3fd29f
Lukasz Puton <lukasputon@gmail.com>
2021-04-05 13:18:33 +0200

    Added Parametrized Tests

--- src/test/java/pl/agh/xp/Lab2/Calculator/CalculatorTest.java
+++ src/test/java/pl/agh/xp/Lab2/Calculator/CalculatorTest.java
@@ -1,18 +1,32 @@
 package pl.agh.xp.Lab2.Calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @Test
-    void testAddUpTwoNumberWhenStringValid(){
+    private static Stream<Arguments> sumTwoValuesProvider() {
+        return Stream.of(
+                Arguments.of("1,2", 3),
+                Arguments.of("-1,3", 2),
+                Arguments.of("1", 1)
+        );
+    }
+
+    @ParameterizedTest(name="{index} => numbersToAdd={0}, sum={1}")
+    @MethodSource("sumTwoValuesProvider")
+    void testAddUpTwoNumberWhenStringValid(String numbersToAdd, int sum){
         Calculator calculator = new Calculator();
 
-        String numbersToAdd = "1,2";
         int result = calculator.add(numbersToAdd);
-        assertEquals(3, result);
+        assertEquals(sum, result);
     }
 
     @Test
