872d5bf19a358abd056935011daab5b2fabb749a
Adrian Janakiewicz <adrjanjan@gmail.com>
2021-04-06 22:28:45 +0200

    #green fourth run

--- src/main/java/pl/agh/xp/Advertisements/StringCalculator/StringCalculator.java
+++ src/main/java/pl/agh/xp/Advertisements/StringCalculator/StringCalculator.java
@@ -5,8 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class StringCalculator {
 
-    public int add(String numbers) {
-        var splitNumbers = numbers.split("[,\n]");
+    public int add(final String numbers) {
+        var delimiters = ",|\n";
+        var processedNumbers = numbers;
+
+        if (numbers.startsWith("//")) {
+            delimiters = delimiters + "|" + numbers.substring(2, numbers.indexOf('\n'));
+            processedNumbers = numbers.substring(numbers.indexOf('\n'));
+        }
+
+        var splitNumbers = processedNumbers.split(delimiters);
         int sum = 0;
         for (String s : splitNumbers) {
             if (!s.isEmpty()) {
