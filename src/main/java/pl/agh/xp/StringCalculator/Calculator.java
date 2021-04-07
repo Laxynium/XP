cc05b3bd873d7d64f14588d578625709f61b815e
grzegorz <grzesiu15432@gmail.com>
2021-04-07 08:03:38 +0200

    Add code passing tests

--- src/main/java/pl/agh/xp/StringCalculator/Calculator.java
+++ src/main/java/pl/agh/xp/StringCalculator/Calculator.java
@@ -5,12 +5,28 @@ import java.util.Arrays;
 
 public class Calculator {
     public String add(String s) {
+        var customDelimiter = detectCustomDelimiter(s);
         var separator = "[,\\n]";
+        if(customDelimiter != null){
+            separator = customDelimiter;
+            s = s.split("\n",2)[1];
+        }
+
         if (s == null || s.isBlank())
             return "0";
         var splitted = s.split(separator);
         var numbers = Arrays.stream(splitted).map(Float::parseFloat);
         var sum = numbers.reduce(0f, Float::sum);
         return new DecimalFormat("0.#").format(sum);
+    }
+
+    private String detectCustomDelimiter(String input){
+        if(input.startsWith("//")){
+            var beforeFirstLine = input.split("\n",2)[0];
+            var separator = beforeFirstLine.replace("//","");
+            return separator;
+        }else{
+            return null;
+        }
     }
 }
