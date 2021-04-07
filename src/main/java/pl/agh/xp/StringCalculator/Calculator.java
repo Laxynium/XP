960b444f2827e2d569f899aca95958888feec784
grzegorz <grzesiu15432@gmail.com>
2021-04-07 19:49:39 +0200

    Make failing test to pass

--- src/main/java/pl/agh/xp/StringCalculator/Calculator.java
+++ src/main/java/pl/agh/xp/StringCalculator/Calculator.java
@@ -5,12 +5,13 @@ import io.vavr.control.Either;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Calculator {
     public Either<String, String> add(String input) {
         var separator = "[,\\n]";
-        if(input.startsWith("//")){
+        if (input.startsWith("//")) {
             String[] split = input.split("\n", 2);
             var beforeFirstLine = split[0];
             separator = beforeFirstLine.replace("//", "");
@@ -19,11 +20,22 @@ public class Calculator {
 
         if (input == null || input.isBlank())
             return Either.right("0");
-        var split = input.split(separator,-1);
-        Supplier<Stream<Either<String,Float>>> parsedNumbers = () -> Arrays.stream(split).map(this::tryParseFloat);
-
-        if(parsedNumbers.get().anyMatch(Either::isLeft)){
+        var split = input.split(separator, -1);
+        Supplier<Stream<Either<String, Float>>> parsedNumbers = () -> Arrays.stream(split)
+                .map(this::tryParseFloat);
+        if (parsedNumbers.get().anyMatch(Either::isLeft)) {
             return Either.left("One of the numbers is invalid.");
+        }
+
+        var negativeNumbers = parsedNumbers.get()
+                .map(Either::get)
+                .filter(x -> x < .0f)
+                .collect(Collectors.toList());
+        if (!negativeNumbers.isEmpty()) {
+            var joined = negativeNumbers.stream().map(x -> new DecimalFormat("0.#")
+                    .format(x))
+                    .collect(Collectors.joining(", "));
+            return Either.left("Negative not allowed : " + joined);
         }
 
         var sum = parsedNumbers.get().map(Either::get).reduce(0f, Float::sum);
@@ -31,11 +43,11 @@ public class Calculator {
         return Either.right(new DecimalFormat("0.#").format(sum));
     }
 
-    private Either<String,Float> tryParseFloat(String input){
-        try{
+    private Either<String, Float> tryParseFloat(String input) {
+        try {
             float value = Float.parseFloat(input);
             return Either.right(value);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             return Either.left(e.getMessage());
         }
     }
