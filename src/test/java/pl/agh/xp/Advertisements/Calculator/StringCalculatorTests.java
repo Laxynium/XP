840a8e2e443120c01015caa18ccabb040a12dee7
Rafal Mazur <rafalek.mazur@gmail.com>
2021-04-09 23:55:47 +0200

    redTests

--- src/test/java/pl/agh/xp/Advertisements/Calculator/StringCalculatorTests.java
+++ src/test/java/pl/agh/xp/Advertisements/Calculator/StringCalculatorTests.java
@@ -2,6 +2,7 @@ package pl.agh.xp.Advertisements.Calculator;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -18,8 +19,7 @@ public class StringCalculatorTests {
             },
             delimiter = ';'
     )
-    void addUpToTwoNumbersWhenStringIsValid(String numbers, int expectedOutput)
-    {
+    void addUpToTwoNumbersWhenStringIsValid(String numbers, int expectedOutput) {
         int result = stringCalculator.add(numbers);
         Assertions.assertEquals(expectedOutput, result);
     }
@@ -35,23 +35,20 @@ public class StringCalculatorTests {
         },
         delimiter = ';'
     )
-    void addUpToAnyNumbersWhenStringIsValid(String numbers, int expectedOutput)
-    {
+    void addUpToAnyNumbersWhenStringIsValid(String numbers, int expectedOutput) {
         int result = stringCalculator.add(numbers);
         Assertions.assertEquals(expectedOutput, result);
     }
 
     @Test
-    void addUpToAnyNumbersUsingNewLineDelimiterWhenStringIsValid()
-    {
+    void addUpToAnyNumbersUsingNewLineDelimiterWhenStringIsValid() {
         String numbers = "12\n200,14";
         int expectedOutput = 226;
         int result = stringCalculator.add(numbers);
         Assertions.assertEquals(expectedOutput, result);
     }
     @Test
-    void addUpToAnyNumbersUsingOnlyNewLineDelimiterWhenStringIsValid()
-    {
+    void addUpToAnyNumbersUsingOnlyNewLineDelimiterWhenStringIsValid() {
         String numbers = "12\n20\n143\n15";
         int expectedOutput = 190;
         int result = stringCalculator.add(numbers);
@@ -59,11 +56,28 @@ public class StringCalculatorTests {
     }
 
     @Test
-    void addUpToAnyNumbersUsingCustomDelimiterWhenStringIsValid()
-    {
+    void addUpToAnyNumbersUsingCustomDelimiterWhenStringIsValid() {
         String numbers = "//;\n1;2";
         int expectedOutput = 3;
         int result = stringCalculator.add(numbers);
         Assertions.assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    void addShouldThrowAnExceptionWhenNegativeNumbersAreUsed1() {
+        String numbers = "1;2;-1;-2";
+        int expectedOutput = 3;
+        Executable result = () -> stringCalculator.add(numbers);
+        Assertions.assertEquals(expectedOutput, result);
+        Assertions.assertThrows(Exception.class, result, "-1, -2");
+    }
+
+    @Test
+    void addShouldThrowAnExceptionWhenNegativeNumbersAreUsed2() {
+        String numbers = "1;2;-1;-2";
+        int expectedOutput = 3;
+        Executable result = () -> stringCalculator.add(numbers);
+        Assertions.assertEquals(expectedOutput, result);
+        Assertions.assertThrows(Exception.class, result, "-1, -2");
     }
 }
