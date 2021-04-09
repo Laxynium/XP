0e9db87e199626636140682ec2b4fc6c7c6ab7b9
Rafal Mazur <rafalek.mazur@gmail.com>
2021-04-09 22:42:26 +0200

    redTests

--- src/test/java/pl/agh/xp/Advertisements/Calculator/StringCalculatorTests.java
+++ src/test/java/pl/agh/xp/Advertisements/Calculator/StringCalculatorTests.java
@@ -1,6 +1,7 @@
 package pl.agh.xp.Advertisements.Calculator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -36,6 +37,23 @@ public class StringCalculatorTests {
     )
     void addUpToAnyNumbersWhenStringIsValid(String numbers, int expectedOutput)
     {
+        int result = stringCalculator.add(numbers);
+        Assertions.assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    void addUpToAnyNumbersUsingNewLineDelimiterWhenStringIsValid()
+    {
+        String numbers = "12\n200,14";
+        int expectedOutput = 226;
+        int result = stringCalculator.add(numbers);
+        Assertions.assertEquals(expectedOutput, result);
+    }
+    @Test
+    void addUpToAnyNumbersUsingOnlyNewLineDelimiterWhenStringIsValid()
+    {
+        String numbers = "12\n20\n143\n15";
+        int expectedOutput = 190;
         int result = stringCalculator.add(numbers);
         Assertions.assertEquals(expectedOutput, result);
     }
