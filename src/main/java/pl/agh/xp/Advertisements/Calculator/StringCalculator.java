155de611a97b8d9be8316b3dead1d0a5d1e38c32
Rafal Mazur <rafalek.mazur@gmail.com>
2021-04-09 23:38:16 +0200

    greenTests

--- src/main/java/pl/agh/xp/Advertisements/Calculator/StringCalculator.java
+++ src/main/java/pl/agh/xp/Advertisements/Calculator/StringCalculator.java
@@ -7,8 +7,15 @@ import java.util.List;
 public class StringCalculator {
 
     public int add(String numbers) {
+        String delimiters = ",|\n";
+        if(numbers.startsWith("//")) {
+            int endOfLine = numbers.indexOf("\n");
+            delimiters = delimiters + "|" + numbers.substring(2,endOfLine);
+            numbers = numbers.substring(endOfLine);
+        }
+
         List<String> splitNumbers = new ArrayList<>();
-        Arrays.stream(numbers.split("[,\n]")).filter(number -> !number.isEmpty()).forEach(splitNumbers::add);
+        Arrays.stream(numbers.split(delimiters)).filter(number -> !number.isEmpty()).forEach(splitNumbers::add);
 
         int sum= 0;
         for(String number : splitNumbers) {
