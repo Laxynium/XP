531069eb8e886c9a8bde60df36459ea8d0bd5b8b
Rafal Mazur <rafalek.mazur@gmail.com>
2021-04-10 12:55:01 +0200

    Part 2 redTests

--- src/main/java/pl/agh/xp/Advertisements/Calculator/StringBitCalculator.java
+++ src/main/java/pl/agh/xp/Advertisements/Calculator/StringBitCalculator.java
@@ -3,6 +3,7 @@ package pl.agh.xp.Advertisements.Calculator;
 public class StringBitCalculator {
     int noOfBits1(String number){
         int sum = 0;
+
         for(int i = 0; i<number.length(); ++i){
             char c = number.charAt(i);
             if(c == '1'){
--- src/test/java/pl/agh/xp/Advertisements/Calculator/StringBitCalculatorTests.java
+++ src/test/java/pl/agh/xp/Advertisements/Calculator/StringBitCalculatorTests.java
@@ -2,6 +2,7 @@ package pl.agh.xp.Advertisements.Calculator;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 public class StringBitCalculatorTests {
 
@@ -9,29 +10,40 @@ public class StringBitCalculatorTests {
 
     @Test
     void countBitsNoNegativeNoDelimiters1(){
-        var result =stringBitCalculator.noOfBits1("");
+        String number = "";
+        var result = stringBitCalculator.noOfBits1(number);
         var expected = 1;
         Assertions.assertEquals(expected,result);
     }
 
     @Test
     void countBitsNoNegativeNoDelimiters2(){
-        var result =stringBitCalculator.noOfBits1("01010101");
+        String number = "01010101";
+        var result = stringBitCalculator.noOfBits1(number);
         var expected = 4;
         Assertions.assertEquals(expected,result);
     }
 
     @Test
     void countBitsNoNegativeNoDelimiters3(){
-        var result =stringBitCalculator.noOfBits1("11111111");
+        String number = "11111111";
+        var result = stringBitCalculator.noOfBits1(number);
         var expected = 8;
         Assertions.assertEquals(expected,result);
     }
 
     @Test
     void countBitsNoNegativeNoDelimiters4(){
-        var result =stringBitCalculator.noOfBits1("00000000");
+        String number = "00000000";
+        var result =stringBitCalculator.noOfBits1(number);
         var expected = 0;
         Assertions.assertEquals(expected,result);
+    }
+
+    @Test
+    void countBitsNoNegativeNoDelimitersExpectThrow() {
+        String number = "0000110011100";
+        Executable result = () -> stringBitCalculator.noOfBits1(number);
+        Assertions.assertThrows(Exception.class, result);
     }
 }
