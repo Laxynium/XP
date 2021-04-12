a56fe6beb5246ed316a8436628000a8fea570754
Adrian Janakiewicz <adrjanjan@gmail.com>
2021-04-12 21:12:10 +0200

    Ex. 2 set-up

---
+++ src/main/java/pl/agh/xp/Advertisements/BitCalculator/BitCalculator.java
@@ -0,0 +1,9 @@
+package pl.agh.xp.Advertisements.BitCalculator;
+
+public class BitCalculator {
+
+    public int noOfBits1(String number){
+        return -1;
+    }
+
+}
---
+++ src/test/java/pl/agh/xp/Advertisements/BitCalculator/BitCalculatorTest.java
@@ -0,0 +1,12 @@
+package pl.agh.xp.Advertisements.BitCalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitCalculatorTest {
+
+    @Test
+    void noOfBits1() {
+    }
+}
