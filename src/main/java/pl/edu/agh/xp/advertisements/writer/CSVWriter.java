8eb034d6ee6cfc47a7c398cbafb1a6c2617b7363
Patryk Bober <boberp26@gmail.com>
2021-05-11 18:49:16 +0200

    Add CSVWriter

---
+++ src/main/java/pl/edu/agh/xp/advertisements/writer/CSVWriter.java
@@ -0,0 +1,51 @@
+package pl.edu.agh.xp.advertisements.writer;
+
+import pl.edu.agh.xp.advertisements.model.Advertisement;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class CSVWriter {
+
+    private static final String headersRow = "\"id\",\"type\",\"format\",\"advertiser\",\"price\",\"price_type\",\"url\",\"title\",\"details\"";
+
+    public void write(String filePath, Advertisement advertisement) {
+        var rowToWrite = convertToCsvRow(advertisement);
+
+        var newFile = false;
+        var fileToWrite = new File(filePath);
+        if (!fileToWrite.exists()) {
+            try {
+                newFile = fileToWrite.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException("Couldn't create a file.");
+            }
+        }
+
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(filePath, true))) {
+            if (newFile) {
+                pw.println(headersRow);
+            }
+            pw.println(rowToWrite);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File has not been found.");
+        }
+    }
+
+    private String convertToCsvRow(Advertisement advertisement) {
+        var fields = Arrays.asList(
+                advertisement.getId().toString(),
+                advertisement.getType(),
+                advertisement.getFormat(),
+                advertisement.getAdvertiser(),
+                advertisement.getPrice(),
+                advertisement.getPrice_type(),
+                advertisement.getUrl(),
+                advertisement.getTitle(),
+                advertisement.getDetails()
+        );
+
+        return "\"" + String.join("\",\"", fields) + "\"";
+    }
+
+}
