7982162101a181978ed09aab22c9462eef1a8961
Patryk Bober <boberp26@gmail.com>
2021-05-12 14:54:37 +0200

    Refactoring

--- src/test/java/pl/edu/agh/xp/advertisements/csv/CSVReaderTest.java
+++ src/test/java/pl/edu/agh/xp/advertisements/csv/CSVReaderTest.java
@@ -1,10 +1,29 @@
 package pl.edu.agh.xp.advertisements.csv;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pl.edu.agh.xp.advertisements.model.Advertisement;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CSVReaderTest {
+
+    @TempDir
+    File tempDir;
 
     @Test
     void read_shouldThrowAnException_whenWrongFileType() {
@@ -12,7 +31,43 @@ public class CSVReaderTest {
 
         Executable executable = () -> CSVReader.read(fileName);
 
-        Assertions.assertThrows(Exception.class, executable, "Wrong file format!");
+        assertThrows(Exception.class, executable, "Wrong file format!");
+    }
+
+    @Test
+    void read_shouldReturnListOfAdvertisements_whenCorrectInputGiven() throws IOException {
+        // given
+        var originalPath = Paths.get("src/test/resources/advertisements.csv");
+        var data = new File(tempDir, "data.csv");
+        Files.copy(originalPath, data.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        assertThat(data).exists();
+        assertEquals(Files.readAllLines(originalPath), Files.readAllLines(data.toPath()));
+
+        // when
+        List<Advertisement> actual = CSVReader.read(data.getAbsolutePath());
+
+        // then
+        assertEquals(Files.readAllLines(data.toPath()).size() - 1, actual.size());
+    }
+
+    @ParameterizedTest
+    @MethodSource("incorrectFilenameInput")
+    void read_shouldThrowAnException_whenFilenameIsNullOrEmpty(String filename) {
+        // given
+
+        // when
+        Exception e = assertThrows(RuntimeException.class, () -> CSVReader.read(filename));
+
+        // then
+        assertEquals("Incorrect filename", e.getMessage());
+    }
+
+    private static Stream<Arguments> incorrectFilenameInput() {
+        return Stream.of(
+                Arguments.arguments((Object) null),
+                Arguments.arguments("")
+        );
     }
 
 }
